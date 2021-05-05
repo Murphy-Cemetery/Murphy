a54463537406d1ccdc028674379f487a6213f4ed
David Huck <dnhuck@dmacc.edu>
2021-05-05 14:27:56 -0500

    added to contact and changed layout for mvc

--- public/resident-list.php
+++ public/resident-list.php
@@ -1,6 +1,4 @@
 <?php
-session_cache_limiter('none');  //This prevents a Chrome error when using the back button to return to this page.
-session_start();
 require_once('../inc/Residents.class.php');
 
 $resident = new Residents();
@@ -18,19 +16,13 @@ $articleList = $newsArticle->getList(
 var_dump($articleList);die;
 */
 
-// $burialList = $resident->getList(
-//     (isset($_GET['firstName']) ? $_GET['firstName'] : null)
-//     // (isset($_GET['middleName']) ? $_GET['middleName'] : null),
-//     // (isset($_GET['lastName']) ? $_GET['lastName'] : null)
-// );
-//$burialList = array();var_dump($_POST);
-$residentList= $resident->getList(
-    (isset($_POST['firstName']) ? $_POST['firstName'] : null),
-    (isset($_POST['lastName']) ? $_POST['lastName'] : null),
-    (isset($_POST['deathYear']) ? $_POST['deathYear'] : null)
-    );
-//$burialList = $resident->burialsData;var_dump($burialList);
-//var_dump($burialList);
+$residentList = $resident->getList(
+    (isset($_GET['firstName']) ? $_GET['firstName'] : null)
+    // (isset($_GET['middleName']) ? $_GET['middleName'] : null),
+    // (isset($_GET['lastName']) ? $_GET['lastName'] : null)
+);
 
-require_once("../tpl/resident-list.tpl.php");
+//var_dump($articleList);
+
+require_once("../tpl/article-list.tpl.php");
 ?>
